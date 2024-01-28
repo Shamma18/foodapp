145230ae817bb59894c3da301df77b584081df50
Shamma18 <118926699+Shamma18@users.noreply.github.com>
2024-01-28 16:59:32 +0530

    some more frontend and navbar changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,30 +1,35 @@
 import React from "react";
 
 export default function Card(props) {
-  const options = props.options || {};
+  // (options)
+
+let options= props.options;
+let priceOptions=Object.keys(options);
+
 
   return (
     <div>
       <div>
-        <div className="card mt-3" style={{ width: "18rem", maxHeight: "360px" }}>
-          <img src="..." className="card-img-top" alt="..." />
+        <div className="card mt-3"
+          style={{ "width": "18rem;", "maxHeight": "360px" }} >
+          <img src={props.imgSrc} className="card-img-top" alt="..." style={{height:"120px" ,objectFit:"fill"}}/>
           <div className="card-body">
             <h5 className="card-title">{props.foodName}</h5>
-
+        
             <div className="container w-100">
-              <select className="m-2 h-100 bg-success">
-                {Array.from(Array(6), (e, i) => (
-                  <option key={i + 1} value={i + 1}>
-                    {i + 1}
-                  </option>
-                ))}
+              <select className="m-2 h-100  bg-success">
+                {Array.from(Array(6), (e, i) => {
+                  return (
+                    <option key={i + 1} value={i + 1}>
+                      {i + 1}
+                    </option>
+                  );
+                })}
               </select>
               <select className="m-2 h-100 bg-success rounded">
-                {Object.keys(options).map((key) => (
-                  <option key={key} value={options[key]}>
-                    {options[key]}
-                  </option>
-                ))}
+               {priceOptions.map((data)=>{
+                return<option key={data} value={data}>{data}</option>
+               })}
               </select>
               <div className="d-inline h-100 fs-5">total price</div>
             </div>
